7065b1ba6a5f511e62adad682c3c2b9532395fc3
Achille kabasele <pepexykabasele@gmail.com>
2022-07-18 12:11:07 +0200

    Second commit

--- Relative_layout/README.md
+++ Relative_layout/README.md
@@ -5,3 +5,28 @@ RelativeLayout permet de définir des coordonnées relatives pour les enfants. S
 RelativeLayout se comporte comme le FloatLayout sauf que ses widgets enfants sont positionnés par rapport à la mise en page.
 
 Lorsqu'un widget avec position = (0,0) est ajouté à un Relative Layout , le widget enfant se déplace également lorsque la position du RelativeLayout est modifiée. Les coordonnées des widgets enfants restent (0,0) car elles sont toujours relatives à la mise en page parent.
+
+
+1. Cette disposition fonctionne de la même manière que FloatLayout, mais les propriétés de positionnement (x, y, center_x, right, y, center_y et top) sont relatives à la taille de la disposition et non à la taille de la fenêtre.
+
+2. En réalité quel que soit le positionnement absolu et relatif, les widgets sont déplacés lorsque la position de la mise en page change.
+Lorsque le widget avec position=(0, 0) est ajouté à RelativeLayout, maintenant, si la position de RelativeLayout est modifiée, le widget enfant se déplacera également. Les coordonnées du widget enfant restent les mêmes, c’est-à-dire (0, 0) car elles sont toujours relatives à la disposition parent.
+
+3. Les touches pos_hint disponibles (x, center_x, right, y, center_y et top) sont utiles pour l’alignement sur les bords ou le centrage.
+
+## Par exemple :
+```pos_hint : {‘center_x’:.5, ‘center_y’:.5}``` alignerait un widget au milieu, quelle que soit la taille de la fenêtre.
+
+
+## Remarque :
+
+Cette disposition vous permet de définir des coordonnées relatives pour les enfants. Si vous voulez un positionnement absolu, utilisez le FloatLayout. Dans RelativeLayout, la taille et la position de chaque widget enfant doivent être données. Cela fait également le placement dynamique.
+
+
+```We can do relative positioning by:
+pos_hint: provide hint of position
+
+We can define upto 8 keys i.e. it takes arguments in form of dictionary.
+
+pos_hint = {“x”:1, “y”:1, “left”:1, “right”:1, "center_x":1, "center_y":1,
+           “top”:1, “bottom”:1("top":0)} ```
--- Relative_layout/main.py
+++ Relative_layout/main.py
@@ -2,59 +2,75 @@ import os
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
-# Sample Python application demonstrating the
-# working of RelativeLayout in Kivy
- 
+# Sample Python application demonstrating the 
+# working of RelativeLayout in Kivy 
+  
 # import modules
-import kivy
- 
-# base Class of your App inherits from the App class.
-# app:always refers to the instance of your application
-from kivy.app import App
- 
-# creates the button in kivy
-# if not imported shows the error
-from kivy.uix.button import Button
- 
+import kivy 
+  
+# base Class of your App inherits from the App class. 
+# app:always refers to the instance of your application 
+from kivy.app import App 
+  
+# creates the button in kivy 
+# if not imported shows the error 
+from kivy.uix.button import Button 
+  
 # This layout allows you to set relative coordinates for children.
 from kivy.uix.relativelayout import RelativeLayout
- 
-# To change the kivy default settings
-# we use this module config
-from kivy.config import Config
-     
-# 0 being off 1 being on as in true / false
-# you can use 0 or 1 && True or False
-Config.set('graphics', 'resizable', True)
- 
-# creating the App class
-class MyApp(App):
- 
+  
+# To change the kivy default settings 
+# we use this module config 
+from kivy.config import Config 
+      
+# 0 being off 1 being on as in true / false 
+# you can use 0 or 1 && True or False 
+Config.set('graphics', 'resizable', True) 
+  
+# creating the App class 
+class Relative_Layout(App):
+      
     def build(self):
- 
-        # creating Relativelayout
-        Rl = RelativeLayout()
- 
+  
+        # creating Relativelayout 
+        rl = RelativeLayout()
+        
         # creating button
-        # a button 30 % of the width and 20 %
-        # of the height of the layout and
-        # positioned at (x, y), you can do
-        # The position does not depend on window size
-        # it just positioned at the given places:
-        btn = Button(text ='Hello world',
-                 size_hint =(.2, .2),
-                 pos =(396.0, 298.0))
-        btn1 = Button(text ='Hello world !!!!!!!!!',
-                 size_hint =(.2, .2),
-                 pos =(-137.33, 298.0))
- 
-        # adding widget i.e button
-        Rl.add_widget(btn)
-        Rl.add_widget(btn1)
-                 
-        # return the layout
-        return Rl
- 
-# run the App
-if __name__ == "__main__":
-    MyApp().run()
+        # size of button is 20 % by hight and width of layout
+        # position is bottom left i.e x = 0, y = 0
+        b1 = Button(size_hint =(.2, .2),
+                    pos_hint ={'x':0, 'y':0},
+                    text ="B1")
+          
+        # position is bottom right i.e right = 1, y = 0
+        b2 = Button(size_hint =(.2, .2),
+                    pos_hint ={'right':1, 'y':0},
+                    text ="B2")
+  
+        b3 = Button(size_hint =(.2, .2),
+                    pos_hint ={'center_x':.5, 'center_y':.5},
+                    text ="B3")
+  
+        b4 = Button(size_hint =(.2, .2),
+                    pos_hint ={'x':0, 'top':1},
+                    text ="B4")
+  
+        b5 = Button(size_hint =(.2, .2),
+                    pos_hint ={'right':1, 'top':1},
+                    text ="B5")
+          
+          
+  
+        # adding button to widget
+        rl.add_widget(b1)
+        rl.add_widget(b2)
+        rl.add_widget(b3)
+        rl.add_widget(b4)
+        rl.add_widget(b5)
+      
+          
+        # returning widget
+        return rl
+# run the App 
+if __name__  == "__main__":
+    Relative_Layout().run()
