0d78c7d2de74fcf765fcd82f60b7dca52509895f
Achille kabasele <pepexykabasele@gmail.com>
2022-07-21 19:14:00 +0200

    Third commit

--- Layouts/Page_layout/main.py
+++ Layouts/Page_layout/main.py
@@ -2,43 +2,66 @@ import os
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
-# code to show how to use StackLayout
- 
-# import kivy module
+# Sample Python application demonstrating 
+# How to create PageLayout in Kivy
+   
 import kivy
- 
-# this restricts the kivy version i.e
-# below this kivy version you cannot
-# use the app or software
-kivy.require("1.9.1")
    
-# base Class of your App inherits from the App class.
-# app:always refers to the instance of your application
+# base Class of your App inherits from the App class.  
+# app:always refers to the instance of your application  
 from kivy.app import App
-   
-# creates the button in kivy
-# if not imported shows the error
+ 
+# The PageLayout class is used to create
+# a simple multi-page layout,
+# in a way that allows easy flipping from
+# one page to another using borders.
+from kivy.uix.pagelayout import PageLayout
+ 
+# creates the button in kivy 
+# if not imported shows the error 
 from kivy.uix.button import Button
+   
+class PageLayout(PageLayout):
+    """
+    Define class PageLayout here
+    """
+  
+    def __init__(self):
+         
+        # The super function in Python can be
+        # used to gain access to inherited methods
+        # which is either from a parent or sibling class.
+        super(PageLayout, self).__init__()
  
-# The StackLayout arranges children vertically
-# or horizontally, as many as the layout can fit.
-from kivy.uix.stacklayout import StackLayout
-
+        # creating buttons on different pages
+        btn1 = Button(text ='Page 1')
+         
+        btn2 = Button(text ='Page 2')
+  
+        btn3 = Button(text ='Page 3')
  
-
-class StackExamen(StackLayout):
-    # Different orientation
-    # ['lr-tb', 'tb-lr', 'rl-tb', 'tb-rl',
-    #  [ 'lr-bt', 'bt-lr', 'rl-bt', 'bt-rl']
-    pass
+        # adding button on the screen
+        # by add widget method
+        self.add_widget(btn1)
  
-# class in which we are creating StackLayout
-class StackApp(App):
-       
-   pass
+        self.add_widget(btn2)
  
-# run function runs the whole program
-# i.e run() method which calls the
-# target function passed to the constructor.
+        self.add_widget(btn3)
+  
+ 
+# creating the App class
+class Page_LayoutApp(App):
+    """
+    App class here
+    """
+  
+    def build(self):
+        """
+        build function here
+        """
+        return PageLayout()
+  
+ 
+# Run the App
 if __name__ == '__main__':
-    StackApp().run()
+    Page_LayoutApp().run()
