2de940ebbe272106fbbd80c0de3a27305268022a
Achille kabasele <pepexykabasele@gmail.com>
2022-08-06 16:44:52 +0200

    sixteenth

--- django_kivy/chatApp/views.py
+++ django_kivy/chatApp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializer import *
+from .models import *
 
 # Create your views here.
+@api_view(['GET'])
+def all_tasks(request):
+    friend = Friend.objects.all()
+    serializer = ChatSerializer(friend, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def create_task(request):
+    data = request.data
+    serializer =ChatSerializer(data=data)
+    if serializer.is_valid():
+        return Response(serializer.data, status=status.HTTP_200_OK)
