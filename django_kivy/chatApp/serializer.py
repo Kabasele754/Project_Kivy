d8c32221926577d89fd37e9b9f10509815b62326
Achille kabasele <pepexykabasele@gmail.com>
2022-08-11 18:34:43 +0200

    twetyth

--- django_kivy/chatApp/serializer.py
+++ django_kivy/chatApp/serializer.py
@@ -1,7 +1,18 @@
+from email.message import Message
 from rest_framework import serializers
 from .models import *
 
-class ChatSerializer(serializers.ModelSerializer):
+class ProfilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        
+class FriendSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friend
+        fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatMessage
         fields = '__all__'
