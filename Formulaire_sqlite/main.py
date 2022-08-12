11cb904882ac9d44bf7a7c3b42c5a198e262153d
Achille kabasele <pepexykabasele@gmail.com>
2022-08-12 15:49:05 +0200

    twety first

--- Formulaire_sqlite/main.py
+++ Formulaire_sqlite/main.py
@@ -1,11 +1,15 @@
 import os
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
-
+import sqlite3
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
 Window.size = (350,550)
+
+def filedialog():
+    global get_image
+    #get_image = 
 
 
 class Enregister(MDApp):
