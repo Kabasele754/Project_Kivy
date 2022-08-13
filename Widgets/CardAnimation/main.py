91ac7fb2ac40dcc200fe9a7ab2f0637ed19392c1
Achille kabasele <pepexykabasele@gmail.com>
2022-08-13 14:22:57 +0200

    twenty second

--- Widgets/CardAnimation/main.py
+++ Widgets/CardAnimation/main.py
@@ -1,10 +1,14 @@
 import imp
 import os
+from platform import win32_edition
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
+from kivy.core.window import Window
+
+Window.size = (300,600)
 
 class CardAnimation(MDApp):
     def build(self):
